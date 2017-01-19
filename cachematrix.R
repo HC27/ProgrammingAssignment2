9baa1790e11956626d8ebbdf92b9670f16665c5b
HC <hc8243@gmail.com>
2017-01-19 14:53:24 +0000

    week 3 exercise - cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to cache matrix inversing, will store the last inverse in the cache
+#and either return this or if the matrix has changed compute and store a new
+#inverse
 
-## Write a short comment describing this function
+## creates a special matrix which stores its own inverse in the cache
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  i <- NULL
+  set <- function(y){
+    x <<-y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inv) i<<-inv
+  getinverse <- function() i
+  list(set=set, get=get,
+       setinverse=setinverse, getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## uses makeCacheMatrix to eitehr compute teh inverse of a matrix or retrieve it
+##from the cache if this has already been calculated
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  y <- x$get()
+  #if(y==x){
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+    # } 
+  }
+  
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
